aa96eabf1be0b5a4dd52489865431890e12f45ab
rerereanl <renaldi232701@gmail.com>
2022-04-09 20:09:21 +0700

    pertemuan-6

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,75 +1,38 @@
+import Movie from "../movie/Movie";
 import styles from "./Movies.module.css";
+import data from "../../utils/constants/data";
+import {useState} from "react";
+import {nanoid} from "nanoid"
 
 function Movies(){
+
+    const [movies, setMovies] = useState(data);
+
+    function tambahFilm(){
+        const movie = {
+            id: nanoid(10),
+            title: "Peaky Blinders",
+            year: 2013,
+            type: "Series",
+            poster: "https://picsum.photos/300/400"
+        }
+        setMovies([...movies, movie]);
+    }
     return (
         <div className = {styles.container}>
             <section className = {styles.movies}> 
                 <h2 className = {styles.movies__title}>Best Movie</h2>
                 <div className = {styles.movie__container}>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
-                    <div className = {styles.movie}>
-                        <img className = {styles.movie__image} src="https://picsum.photos/300/400" alt="" />
-                        <h3 className = {styles.movie__title}>Title Movie</h3>
-                        <p className = {styles.movie__date}>5 Maret 2022</p>
-                    </div>
+                    {
+                        // looping data dengan function map
+                        // mengirim props ke movie
+                        movies.map(function(movie){
+                           return <Movie key = {movie.id} movie = {movie} />
+                        })
+                    }
+                    
                 </div>
-                
-                
-
+                <button className = {styles.button__add__movie} onClick = {tambahFilm}>Add Movie</button>
             </section>
         </div>
     );
